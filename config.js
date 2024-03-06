a0b977ac57c47459a9e46692d724a2d7b445611c
Isaacbls <161089468+Isaacbls@users.noreply.github.com>
2024-03-06 10:16:17 -0500

    NUEVOS

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
+  ['51981368587', '👑 Mystic - Creador 👑', true],
   ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
   ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
   ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
@@ -22,32 +22,32 @@ global.owner = [
   ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
   ['393518398856','💫 Mystic - Tester Fabri115💫', true],
   ['593968585283'],
-  ['5219993404349'],
-  ['5219991402134'],
+  ['0000000000000'],
+  ['0000000000000'],
   ['5492266466080'],
-  ['5219996125657'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50497150165'],
-  ['51906662557'],
-  ['573183650526'], 
-  ['5217441298510'], 
-  ['5217294888993'],
-  ['595992611272']
+  ['0000000000000'],
+  ['0000000000000'],
+  ['0000000000000'],
+  ['0000000000000'], 
+  ['0000000000000'],  
+  ['0000000000000'],
+  ['0000000000000'],
+  ['0000000000000'],
+  ['0000000000000'], 
+  ['0000000000000'], 
+  ['0000000000000'],
+  ['0000000000000']
 ];
 
 global.suittag = ['5219993404349'];
 global.prems = ['51995386439'];
 
 global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
-global.wm = 'The Mystic - Bot';
-global.titulowm = 'The Mystic - Bot';
-global.titulowm2 = `The Mystic - Bot`
-global.igfg = 'The Mystic - Bot';
+global.author = 'The kobeni - Bot';
+global.wm = 'The kobeni - Bot';
+global.titulowm = 'The kokeni - Bot';
+global.titulowm2 = `The kobeni - Bot`
+global.igfg = 'The kobeni - Bot';
 global.wait = '*[ ⏳ ] Cargando...*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
@@ -69,8 +69,8 @@ global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
 global.wm2 = `${dia} ${fecha}\nThe Mystic - Bot`;
-global.gt = 'The Mystic - Bot';
-global.mysticbot = 'The Mystic - Bot';
+global.gt = 'The kobeni - Bot';
+global.mysticbot = 'The kobeni - Bot';
 global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
 global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
 global.waitt = '*[ ⏳ ] Cargando...*';
